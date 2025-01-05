4f306d06a47c8f2ad7502eb25c433c90d9520ec5
jarvis.li <332976194@qq.com>
2025-01-05 13:28:29 +0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,13 @@
+require("config.lazy")
+
+-- keymaps
+require("keymaps.keymaps")
+require("keymaps.nvim-tree")
+require("keymaps.hop")
+
+-- settings
+require("settings.settings")
+require("settings.omnisharp")
+require("settings.hop")
+require("settings.colorscheme")
+require("settings.undotree")
