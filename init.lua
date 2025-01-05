ef811197e4747af16949faf50e2afaeab7a14607
jarvis.li <332976194@qq.com>
2025-01-05 18:29:05 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -8,6 +8,5 @@ require("keymaps.hop")
 -- settings
 require("settings.settings")
 require("settings.omnisharp")
-require("settings.hop")
 require("settings.colorscheme")
 require("settings.undotree")
